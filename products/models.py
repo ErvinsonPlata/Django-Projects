0af5790fa28cd73d07040d016de57b4180c93b1c
Ervison Plata <ervinsonplata16@gmail.com>
2024-12-11 16:07:13 -0500

    Add Products

---
+++ products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    name = models.TextField(max_length=100, verbose_name="Nombre")
+    description = models.TextField(max_length=300, verbose_name="Descripción")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
+    avilable = models.BooleanField(default=True, verbose_name="Disponible")
+    photo = models.ImageField(
+        upload_to="logos", null=True, blank=True, verbose_name="Foto"
+    )
+
+    def __str__(self):
+        return self.name
