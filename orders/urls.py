e966071e49bccc9d2c4f37edeb729f01e6cdd9ae
Ervison Plata <ervinsonplata16@gmail.com>
2024-12-19 18:29:32 -0500

    Add view Users

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from orders.views import MyOrderView
+from .views import CreateOrderProductView
 
 urlpatterns = [
 
     path("mi-orden/", MyOrderView.as_view(), name="my_order"),
-
+    path('agregar-producto',CreateOrderProductView.as_view(), name="add_product"),
 
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,8 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView,CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin #para que el usuario tenga que estar logeado
-
+from django.urls import reverse_lazy
 from .models import Order
+from .forms import OrderProductForm
 # Create your views here.
 
 class MyOrderView(LoginRequiredMixin,DetailView):
@@ -13,3 +14,18 @@ class MyOrderView(LoginRequiredMixin,DetailView):
 
         return Order.objects.filter(user= self.request.user, is_active=True).first()
     
+
+class CreateOrderProductView(LoginRequiredMixin,CreateView):
+    template_name = "orders/my_order.html"
+    form_class = OrderProductForm
+    success_url = reverse_lazy("my_order")
+
+    def form_valid(self, form):
+        order,_= Order.objects.get_or_create(
+            is_active=True,
+            user=self.request.user,
+        )
+        form.instance.order = order
+        form.instance.quantity = 1
+        form.save()
+        return super().form_valid(form)
